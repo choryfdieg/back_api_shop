8521c28b42e0d735be35002959f5fbd0e3b32707
choryfdieg <choryfdieg@gmail.com>
2020-03-06 15:50:43 -0500

    first commit

---
+++ app/User.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    const USUARIO_VERIFICADO = '1';
+    const USUARIO_NO_VERIFICADO = '0';
+
+    const USUARIO_ADMINISTRADOR = 'true';
+    const USUARIO_REGULAR = 'false';
+
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 
+        'email', 
+        'password',
+        'verified',
+        'verification_token',
+        'admin'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 
+        'remember_token',
+        'verification_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function esVerificado(){
+        return $this->verified == User::USUARIO_VERIFICADO;
+    }
+
+    public function esAdministrador(){
+        return $this->admin == User::USUARIO_ADMINISTRADOR;
+    }
+
+    public static function generarVerificacionToken(){
+        return Str::random(40);
+    }
+
+    public function setNameAttribute($valor){
+        $this->attributes['name'] = strtolower($valor);
+    }
+
+    public function setEmailAttribute($valor){
+        $this->attributes['email'] = strtolower($valor);
+    }
+
+    public function getNameAttribute($valor){
+        return ucfirst($valor);
+    }
+}
